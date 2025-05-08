f46b6f5bb740f0a0623c4f749cbbd754279444ed
现阶段：秀发 <96060743+g156445@users.noreply.github.com>
2025-05-08 19:43:07 +1000

    update game_continue

---
+++ game_continue.py
@@ -0,0 +1,26 @@
+'''
+Track the current level and determine if there is a next level:
+    If there is a next level, current_level + 1, and return True.
+    If it is the last level, issue the key (has_key=True), output the prompt, and return False.
+'''
+
+class game_continue:
+    def __init__(self):
+        self.current_level = 1      # set starting from 1
+        self.total_levels = {
+            'SIMPLE': 1,
+            'NORMAL': 3,
+            'HELL': 5
+        }
+        self.has_key = False
+
+    def continue_maze(self, difficulty: str) -> bool:
+        if self.current_level < self.total_levels[difficulty.upper()]:
+            self.current_level += 1
+            # If there is a next level, continue into the maze.
+            return True
+        else:
+            self.has_key = True
+            print("obtained the key")
+            # If there is no next level, clear the level
+            return False
