ec69cf1b1df6c10088f9c4918f8db48cc53277bc
现阶段：秀发 <96060743+g156445@users.noreply.github.com>
2025-05-08 17:56:53 +1000

    create player class and monster class

---
+++ player_monster.py
@@ -0,0 +1,26 @@
+# define player and monster class
+'''
+1. Player class:
+- Represents the player character, basic health, attack power, coordinate position and whether it holds a key.
+- Starting from the starting point of the map, improve attack power through fighting and answering questions, and the ultimate goal is to escape the maze.
+
+2. Monster class:
+- Represents the monsters in each room of the map, randomly generates health, and attack power can be passed in according to the difficulty setting.
+- Each monster will appear once in a room.
+'''
+import random
+
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.x = 0
+        self.y = 0
+        self.attack = 0
+        self.hp = 10
+        self.has_key = False
+
+class Monster:
+    def __init__(self, attack: int):
+        self.hp = random.randint(10, 20)    #random set monster hp 10-20 in each map
+        self.attack = attack
